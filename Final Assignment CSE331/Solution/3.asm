ab34d9b32ba8068504576b48f161073ff2f32fdf
1731201042 - Shajib <sakib.shajib173@northsouth.edu>
2021-01-03 17:42:42 +0600

    Final Assignment Completed

--- Final Assignment CSE331/Solution/3.asm
+++ Final Assignment CSE331/Solution/3.asm
@@ -1,42 +1,42 @@
-.MODEL SMALL 
-.STACK 100H
+.MODEL SMALL
+ .STACK 100H
 
- .DATA    
- PROMPT_1  DB  "Original value of AL : $"
-    PROMPT_2  DB  0DH,0AH,"Processed value of AL : $" 
+ .DATA
+    PROMPT_1  DB  'Original value of AL : $'
+    PROMPT_2  DB  0DH,0AH,'Processed value of AL : $'
 
-.CODE 
-MAIN PROC
+ .CODE
+   MAIN PROC
      MOV AX, @DATA                ; initialize DS
      MOV DS, AX
 
-     LEA DX, PROMPT_1             ; load and print PROMPT_1     
-MOV AH, 9     
-INT 21H     
+     LEA DX, PROMPT_1             ; load and print PROMPT_1
+     MOV AH, 9
+     INT 21H
 
-MOV AL, 38H                  ; place 8 in the AL     
-MOV BL, AL                   ; save Al in to BL         
-MOV AH, 2                    ; print the original value of AL     
-MOV DL, AL     
-INT 21H     
+     MOV AL, 31H                  ; place 1 in the AL
+     MOV BL, AL                   ; save AL in to BL    
 
-LEA DX, PROMPT_2             ; load and print PROMPT_2     
-MOV AH, 9     
-INT 21H     
+     MOV AH, 2                    ; print the original value of AL
+     MOV DL, AL
+     INT 21H
 
-AND BL, 0FH                  ; convert ascii to decimal code     
+     LEA DX, PROMPT_2             ; load and print PROMPT_2
+     MOV AH, 9
+     INT 21H
 
-MOV CL, 2                    ; divide AL by 4     
-SHL BL, CL     
+     AND BL, 0FH                  ; convert ascii to decimal code 
 
-OR BL, 30H                   ; convert decimal to ascii code
+     MOV CL, 2                    ; multiply AL by 4
+     SHL BL, CL
+
+     OR BL, 30H                   ; convert decimal to ascii code
 
      MOV AH, 2                    ; print the processed value of AL
-   MOV DL, BL
-  INT 21H   
+     MOV DL, BL
+     INT 21H
 
-MOV AH, 4CH                  ; return control to DOS     
-INT 21H
-
-   MAIN ENDP
-END MAIN
+     MOV AH, 4CH                  ; return control to DOS
+     INT 21H
+   MAIN ENDP 
+ END MAIN
